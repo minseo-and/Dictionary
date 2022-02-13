fad731c533da0c8b135ce5575f1678370ef16de2
minseo <msp05070478@gmail.com>
2022-02-13 15:24:26 +0900

    replace

--- app/src/main/java/com/example/dictionary/Kotlin/KotlinActivity.kt
+++ app/src/main/java/com/example/dictionary/Kotlin/KotlinActivity.kt
@@ -13,6 +13,10 @@ class KotlinActivity : AppCompatActivity() {
         val a = 1
         var b = 2
 
+        val str = "코로나 조심하세요"
+        val str2 = str.replace("조삼하세요", "이겨내요")
+        // replace를 사용하면 문자형 수정이 가능하다
+
         // val은 값이 변하지 못하고 var는 가능하다
         // 변수형을 생략하여도 된다
         println(sum(1, 2))
